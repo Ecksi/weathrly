0c72525f3fe6d04192b0e3e8854a8ceffbb1a6e5
Ecksi <jimmy.dechant@gmail.com>
2018-04-14 09:23:08 -0600

    Refactor SevenHour- move conditional to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './styles/css/Card.css';
 
 const Card = ({ hour, condition, image, temp, call, day, tempHigh, tempLow }) => {
+  if (hour === '12') {
+    hour = `${hour}:00 PM`;
+  } else if (hour > 11) {
+    hour = `${hour - 12}:00 PM`;
+  } else if (hour === '0') {
+    hour = `12:${hour}0 AM`;
+  } else {
+    hour = `${hour}:00 AM`;
+  }
+
   if (call === '7-hour') {
     return (
       <div className="Card">
         <h2 className="cardHeader">{hour}</h2>
         <img src={image} alt="weather" />
-        <h3>{temp}</h3>
+        <h3 className="hourly-temp">{temp}&#176;</h3>
       </div>
     );
   } else {
@@ -16,8 +26,8 @@ const Card = ({ hour, condition, image, temp, call, day, tempHigh, tempLow }) =>
         <h2 className="cardText cardHeader">{day}</h2>
         <img src={image} alt="weather" />
         <div className="ten-day-high-low">
-          <h3 className="cardText cardHigh">{tempHigh}F</h3>
-          <h3 className="cardText cardLow">{tempLow}F</h3>
+          <h3 className="cardText cardHigh">{tempHigh}&#176;</h3>
+          <h3 className="cardText cardLow">{tempLow}&#176;</h3>
         </div>
       </div>
     );
