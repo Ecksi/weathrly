35136c09fae39aead63458cb24dcdfee818f50a2
Ecksi <jimmy.dechant@gmail.com>
2018-04-16 10:11:50 -0600

    Add 404 functionality if city or state is not found

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Search from './Search';
 import WeatherComponent from './WeatherComponent';
 import {getCurrentWeatherForecast, getSevenHourForecast, getTenDayForecast } from '../ApiService';
 import getForecasts from '../apiCall';
+import ErrorPage from './404-error'
 import './styles/css/App.css';
 
 class App extends Component {
@@ -11,9 +12,10 @@ class App extends Component {
     super();
     this.state = {
       sevenHourData: null,
-      tenDayData: null, 
+      tenDayData: null,
       currentWeatherData: null,
-      showWelcome: true
+      showWelcome: true,
+      error: false
     };
     
     this.apiCall = this.apiCall.bind(this);
@@ -22,27 +24,33 @@ class App extends Component {
   componentDidMount() {
     let localStorageWeather = localStorage.getItem('weather');
     let userWeatherHistory = JSON.parse(localStorageWeather);
-    
-    if(userWeatherHistory) {
+
+    if (userWeatherHistory) {
       this.setState({
         sevenHourData: userWeatherHistory.sevenHourData,
         tenDayData: userWeatherHistory.tenDayData, 
         currentWeatherData: userWeatherHistory.currentWeatherData,
         showWelcome: userWeatherHistory.showWeather
-      })
+      }) 
     }
   }
   
   apiCall({ state, city }) {
     getForecasts(state, city)
       .then(data => {
+        if (Object.keys(data).length === 1) {
+          this.setState({
+            error: true
+          })
+        } else {
         this.setState({ 
           sevenHourData: getSevenHourForecast(data.hourly_forecast),
-          tenDayData: getTenDayForecast(data.forecast.simpleforecast.forecastday), 
+          tenDayData: getTenDayForecast(data.forecast.simpleforecast.forecastday),
           currentWeatherData: getCurrentWeatherForecast(data),
           showWelcome: false
         })
-        localStorage.setItem('weather', JSON.stringify(this.state))       
+        localStorage.setItem('weather', JSON.stringify(this.state))
+        }
       })
   }
   
@@ -53,10 +61,12 @@ class App extends Component {
         sevenHourData={this.state.sevenHourData}
         tenDayData={this.state.tenDayData}
       />
+    const noError = 
+      this.state.showWelcome ? <Welcome /> : showWeather
 
     return (
-      <div className="Weathrly"> 
-        {this.state.showWelcome ? <Welcome /> : [showWeather]}
+      <div className="Weathrly">
+        {this.state.error ? <ErrorPage /> : noError}
         <Search apiCall={this.apiCall} welcome={this.state} />
       </div>
     );
