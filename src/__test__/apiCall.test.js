477a695d26192273eead8fa37e2f60eb7eed3099
Ecksi <jimmy.dechant@gmail.com>
2018-04-16 14:12:48 -0600

    Add apiCall tests

--- src/__test__/apiCall.test.js
+++ src/__test__/apiCall.test.js
@@ -2,8 +2,7 @@ import getForecasts from '../apiCall';
 import {data} from '../mockData';
 
 describe('Api Call', () => {
-
-  it.skip('Should return an object', () => {
+  it('Should return an object', () => {
     expect(typeof getForecasts('wa', 'seattle')).toBe('object');
   })
 
