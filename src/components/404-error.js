bc2a1b77ebc2fcf7552bc0960eb77909127e7649
Sage Vogt <thurmanvogt@gmail.com>
2018-04-16 22:46:04 -0600

    Add css for error page

--- src/components/404-error.js
+++ src/components/404-error.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './styles/css/404Error.css';
 
 const ErrorPage = () => {
   return(
-    <div>
-      <h1>This is our Error Page</h1>
+    <div className="error-page">
+      <h1>404</h1>
+      <h2>Whoops</h2>
+      <h3>You did not enter a valid location</h3>
+      <h4>It'd be a whole lot cooler if you did</h4>
+      <img className="cloud cloud-1" src="./images/cloud.svg" alt="cloud"/>
+      <img className="cloud cloud-2" src="./images/cloud.svg" alt="cloud"/>
+      <img className="cloud cloud-3" src="./images/cloud.svg" alt="cloud"/>
+      <img className="cloud cloud-4" src="./images/cloud.svg" alt="cloud"/>
     </div>
   )
 }
